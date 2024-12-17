0bc640cf0996e1dd29f2383861a6e45416d4954c
Bizu2 <bzuwnda@gmail.com>
2024-12-17 11:55:36 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import ApiDocumentation from "./components/Documentation/ApiDocumentation"
+// import APIDocumentation from "./components/API/APIDocumentation"
+// import Apidoc from "./components/API/Apidoc.jsx";
+
+
+function App() {
+
+
+  return (
+    <>
+      {/* <Documentation /> */}
+      <ApiDocumentation />
+      {/* <APIDocumentation /> */}
+      {/* <Apidoc /> */}
+    </>
+  );
+}
+
+export default App
